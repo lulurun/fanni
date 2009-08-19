9fcdf3a15eebfdb7e0cd64913c2b519a397ac36b
lulurun <lulurun@gmail.com>
2009-08-20 00:51:44 +0900

    improved implementation of fanni/Timer.h

--- include/fanni/Timer.h
+++ include/fanni/Timer.h
@@ -5,6 +5,8 @@
 #include "fanni/ThreadTemplate.h"
 #include "fanni/Logger.h"
 
+// TODO @@@ CPU cost !!, change to a better "TIMER"
+
 namespace Fanni {
 
 class OnTimerElapsedHandler {
@@ -14,57 +16,62 @@ public:
 	virtual void operator()() const = 0;
 };
 
-// TODO @@@ BUG!!!!
-class Timer {
-	clock_t counter;
-public:
-	Timer() {
-		this->start();
-	};
-
-	void start() {
-		this->counter = std::clock();
-	};
-
-	bool elapsed(clock_t ms) {
-		clock_t tick = std::clock();
-		if(tick - this->counter >= ms) {
-			this->counter = tick;
-			return true;
-		}
-		return false;
-	}
-};
-
-class TimeOutTaskBase : public ThreadHandlerBase{
+class TimeOutTaskBase : public ThreadHandlerBase {
+	static const uint64_t NANOSEC_PER_SEC = 1000 * 1000 * 1000;
+	static const uint64_t NANOSEC_PER_MILLISEC = 1000 * 1000;
 protected:
-	clock_t period;
+	class Timer {
+		timespec counter;
+		uint64_t timespec_difference(timespec a, timespec b) {
+			uint64_t nsec_a = (uint64_t)(a.tv_sec) * NANOSEC_PER_SEC + a.tv_nsec;
+			uint64_t nsec_b = (uint64_t)(b.tv_sec) * NANOSEC_PER_SEC + b.tv_nsec;
+			return nsec_b - nsec_a; // TODO @@@ b must be bigger than a
+		}
+
+		void reset() {
+			::clock_gettime(CLOCK_REALTIME, &this->counter);
+		};
+
+	public:
+		Timer() {
+			this->reset();
+		};
+
+		bool elapsed(uint64_t ns) {
+			timespec now;
+			::clock_gettime(CLOCK_REALTIME, &now);
+			if(this->timespec_difference(this->counter, now) > ns) {
+				this->reset();
+				return true;
+			}
+			return false;
+		}
+	};
+
+	uint64_t period_ns;
 	const OnTimerElapsedHandler *handler;
 
 public:
-	TimeOutTaskBase(int ms, const OnTimerElapsedHandler *handler) {
-		this->period = CLOCKS_PER_SEC * 1000 / ms;
+	TimeOutTaskBase(int milliseconds, const OnTimerElapsedHandler *handler) {
+		this->period_ns = milliseconds  * NANOSEC_PER_MILLISEC;
 		this->handler = handler;
 	};
 	~TimeOutTaskBase() {};
-	virtual void start() const = 0;
-
 	// from ThreadHandlerBase
 	virtual void setArg(void *arg) {};
-
-	virtual void operator()() {
-		this->start();
-	}
+	virtual void start() const = 0;
+	virtual void operator()() { this->start(); }
 };
 
+static const uint32_t ELAPSE_CHECK_INTERVAL = 1000 * 1000; /* microseconds, I am using usleep! */
 class OneTimeTask : public TimeOutTaskBase {
 public:
-	OneTimeTask(int ms, const OnTimerElapsedHandler *handler) : TimeOutTaskBase(ms, handler){};
-
+	OneTimeTask(int milliseconds, const OnTimerElapsedHandler *handler) :
+		TimeOutTaskBase(milliseconds, handler){};
 	virtual void start() const {
 		Timer timer;
 		while(1) {
-			if (timer.elapsed(this->period)) {
+			if (timer.elapsed(this->period_ns)) {
 				try {
 					this->handler->operator()();
 				} catch (ErrorException &e) {
@@ -73,6 +80,7 @@ public:
 				}
 				break;
 			}
+			::usleep(ELAPSE_CHECK_INTERVAL);
 		}
 	}
 };
@@ -82,8 +90,8 @@ private:
 	int max_time;
 
 public:
-	PeriodicTask(int ms, const OnTimerElapsedHandler *handler, int max_time = -1) :
-		TimeOutTaskBase(ms, handler), max_time(max_time) { };
+	PeriodicTask(int milliseconds, const OnTimerElapsedHandler *handler, int max_time = -1) :
+		TimeOutTaskBase(milliseconds, handler), max_time(max_time) { };
 
 	void stop() {};
 
@@ -100,7 +108,7 @@ private:
 		Timer timer;
 		int count = 0;
 		while(count < max_time) {
-			if (timer.elapsed(this->period)) {
+			if (timer.elapsed(this->period_ns)) {
 				try {
 					this->handler->operator()();
 					count++;
@@ -110,13 +118,14 @@ private:
 				}
 				// TODO @@@ implement stop !!
 			}
+			::usleep(ELAPSE_CHECK_INTERVAL);
 		}
 	}
 
 	virtual void _start_forever() const {
 		Timer timer;
 		while(1) {
-			if (timer.elapsed(this->period)) {
+			if (timer.elapsed(this->period_ns)) {
 				try {
 					this->handler->operator()();
 				} catch (ErrorException &e) {
@@ -125,6 +134,7 @@ private:
 				}
 				// TODO @@@ implement stop !!
 			}
+			::usleep(ELAPSE_CHECK_INTERVAL);
 		}
 	}
 };
